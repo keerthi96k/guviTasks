71ac0066cb378913e551e667c6b0deb725c7306b
keerthi <keerthikummari@gmail.com>
2020-09-19 22:10:02 +0530

    update

--- Day 3/bowtie.js
+++ Day 3/bowtie.js
@@ -21,3 +21,58 @@ function printAllKeys(obj){
 //console.log(printAllKeys(obj))
 
 //Write a function called “convertObjectToList” which converts an object literal into an array of arrays.
+var object = {name: 'ISRO', age: 35, role: 'Scientist'}
+function convertObjectToList(obj){
+arr=[]
+for(item in obj){
+    arr.push([item,obj[item]])
+}
+return arr;
+}
+//console.log(convertObjectToList(object))
+
+//Parsing a list and transform the first and last elements of it:
+//Write a function ‘transformFirstAndLast’ that takes in an array, and returns an object with:
+//1) the first element of the array as the object’s key, and
+//2) the last element of the array as that key’s value.
+var array = ['GUVI', 'I', 'am', 'a geek'];
+
+function transformFirstAndLast(arr){
+        var obj = {}
+        var k = arr[0]
+        var val = arr[arr.length-1]
+            obj[k] = val   
+         return obj;
+}
+//console.log(transformFirstAndLast(array))
+
+//Write a function “fromListToObject” which takes in an array of arrays, and returns an object with each pair of elements in the array as a key-value pair.
+var array1 = [['make', 'Ford'], ['model', 'Mustang'], ['year', 1964]];
+function fromListToObject(arr){
+    var obj ={}
+    for(let i=0; i<arr.length; i++){
+        obj[arr[i][0]] = arr[i][1]
+    }
+    return obj
+}
+//console.log(fromListToObject(array1))
+
+//Parsing a list of lists and convert into a JSON object:
+//Write a function called “transformGeekData” that transforms some set of data from one format to another.
+var array2 = [[['firstName', 'Vasanth'], ['lastName', 'Raja'], ['age', 24], ['role', 'JSWizard']], 
+[['firstName', 'Sri'], ['lastName', 'Devi'], ['age', 28], ['role', 'Coder']]];
+function transformGeekData(arr){
+    var transform = []
+    for(let i=0; i<arr.length; i++){
+        items = arr[i]
+        obj ={}
+        for(let j=0; j<items.length; j++){
+            obj[items[j][0]] = items[j][1]
+        }
+        transform.push(obj)
+    }
+    return transform;
+}
+console.log(transformGeekData(array2))
+
+//
